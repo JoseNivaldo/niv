5050c1f0938f9a635d774db1f89577b81df3ea80
JoseNivaldo <silva.santos.jose2611@escola.pr.gov.br>
2025-06-05 14:28:19 -0300

    Add files via upload

---
+++ Jnivaldo-main/Jose-2-B-Nivaldo-main/Nivaldo-main/José Nivaldo2B/assets/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao (
+    'Geografia',
+    'Qual o maior país do mundo?',
+    'O maior país é a Russia'
+)
+criaCartao (
+    'Geografia',
+    'Qual o maior país da America do Sul?',
+    resposta
+)
+criaCartao (
+    categoria,
+    pergunta,
+    resposta
+)
+
+
